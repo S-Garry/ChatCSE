eb68c491e037d60244d99daba8f9a8007b9ea917
SG <97837305+S-Garry@users.noreply.github.com>
2025-05-16 15:00:10 +0800

    feat: auth page implementation

---
+++ frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,57 @@
+// src/app/page.tsx
+'use client'
+
+import { useState } from 'react'
+import styles from '@/app/auth/auth.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+    console.log('Logging in with username: ', username,', password: ', password)
+    // 這裡之後會送到 backend
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>Login</h2>
+        <div className="mb-4">
+          <label className={styles.label}>
+            Username
+          </label>
+          <input
+            type="text"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            className={styles.input}
+            placeholder="Enter username"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            className={styles.input}
+            placeholder="Enter password"
+          />
+        </div>
+        <button
+          onClick={handleLogin}
+          className={styles.button}
+        >
+          Login
+        </button>
+        <p className={styles.textLink}>
+          Don't have an account? <Link href="/auth/register">Register here</Link>
+        </p>
+      </div>
+    </div>
+  )
+}
