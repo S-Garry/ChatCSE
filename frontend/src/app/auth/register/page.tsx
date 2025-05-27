cb9b488d4dc7a0dc4c00adc65be8560bad3ebc0e
SG <97837305+S-Garry@users.noreply.github.com>
2025-05-27 15:46:36 +0800

    feat: add OTP page in register

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -8,13 +8,16 @@ import { showSuccess, showError, clearToast } from '@/components/ToastMessage'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [otp, setOtp] = useState('')
   const [isProcessing, setProcessing] = useState(false)
+  const [isOtpPhase, setOtpPhase] = useState(false)
   const router = useRouter()
 
   const handleRegister = () => {
-    console.log('Register with username: ', username,', password: ', password)
+    console.log('Register with username: ', username, ', password: ', password)
     // 這裡之後會送到 backend
 
     if (isProcessing) {
@@ -22,8 +25,8 @@ export default function Home() {
       return
     }
 
-    if (!username || !password) {
-      showError('Username and password are required')
+    if (!username || !password || !email) {
+      showError('Email, username and password are required')
       return
     }
 
@@ -33,11 +36,31 @@ export default function Home() {
       return;
     }
     else {
+      setOtpPhase(true)
+    }
+  }
+
+  const handleOTP = () => {
+    if (isProcessing) {
+      showError('Still in process')
+      return
+    }
+
+    if (!otp) {
+      showError('OTP are required')
+      return
+    }
+
+    if (otp == "12345") {
       setProcessing(true)
       showSuccess('Register Success')
       setTimeout(() => router.push('login'), 1500)
       setTimeout(() => clearToast(), 1450)
       setTimeout(() => setProcessing(false), 1400)
+    }
+    else {
+      showError('Invalid OTP')
+      return;
     }
   }
 
@@ -45,39 +68,73 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.card}>
         <h2 className={styles.title}>Register</h2>
-        <div className="mb-4">
-          <label className={styles.label}>
-            Username
-          </label>
-          <input
-            type="text"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            className={styles.input}
-            placeholder="Enter username"
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Password
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            className={styles.input}
-            placeholder="Enter password"
-          />
-        </div>
-        <button
-          onClick={handleRegister}
-          className={styles.button}
-        >
-          Register
-        </button>
-        <p className={styles.textLink}>
-          Already have an account? <Link href="/auth/login">Login here</Link>
-        </p>
+        {!isOtpPhase ? (
+          <>
+            <div className="mb-4">
+              <label className={styles.label}>
+                Email
+              </label>
+              <input
+                type="text"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className={styles.input}
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="mb-4">
+              <label className={styles.label}>
+                Username
+              </label>
+              <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                className={styles.input}
+                placeholder="Enter username"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className={styles.input}
+                placeholder="Enter password"
+              />
+            </div>
+            <button
+              onClick={handleRegister}
+              className={styles.button}
+            >
+              Register
+            </button>
+            <p className={styles.textLink}>
+              Already have an account? <Link href="/auth/login">Login here</Link>
+            </p>
+          </>
+        ) : (<>
+          <div className="mb-4">
+            <label className={styles.label}>OTP Code</label>
+            <input
+              type="text"
+              value={otp}
+              onChange={e => setOtp(e.target.value)}
+              className={styles.input}
+              placeholder="Enter the OTP"
+            />
+          </div>
+          <button
+            onClick={handleOTP}
+            className={styles.button}
+          >
+            Send
+          </button>
+        </>
+        )}
       </div>
     </div>
   )
